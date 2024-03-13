a52a210b793380f6e24229690702540a4e1c2609
DylanJoo <dylanjootw@gmail.com>
2024-03-13 06:58:04 +0100

    add beir analysis

--- analyze_vocab_overlap.sh
+++ analyze_vocab_overlap.sh
@@ -1,12 +1,12 @@
 dataset_dir=/home/dju/datasets
 
 # Analyze msmarco and other datasets
-python tools/analysis/vocab_overlap.py \
-    --files_path ${dataset_dir}/msmarco/collection/corpus.jsonl \
-    --files_path ${dataset_dir}/beir/scifact/collection/corpus.jsonl \
-    --files_path ${dataset_dir}/beir/scidocs/collection/corpus.jsonl \
-    --files_path ${dataset_dir}/beir/trec-covid/collection/corpus.jsonl \
-    --output_image analysis-beir.png --output_text analysis-beir.txt &
+# python tools/analysis/vocab_overlap.py \
+#     --files_path ${dataset_dir}/msmarco/collection/corpus.jsonl \
+#     --files_path ${dataset_dir}/beir/scifact/collection/corpus.jsonl \
+#     --files_path ${dataset_dir}/beir/scidocs/collection/corpus.jsonl \
+#     --files_path ${dataset_dir}/beir/trec-covid/collection/corpus.jsonl \
+#     --output_image analysis-beir.png --output_text analysis-beir.txt
 
 # Analyze msmarco and other datasets
 python tools/analysis/vocab_overlap.py \
@@ -15,5 +15,5 @@ python tools/analysis/vocab_overlap.py \
     --files_path ${dataset_dir}/lotte/recreation/test/collection/docs00.json \
     --files_path ${dataset_dir}/lotte/science/test/collection/docs00.json \
     --files_path ${dataset_dir}/lotte/technology/test/collection/docs00.json \
-    --files_path ${dataset_dir}/lotte/writing/test/collection/docs00.json 
+    --files_path ${dataset_dir}/lotte/writing/test/collection/docs00.json \
     --output_image analysis-lotte.png --output_text analysis-lotte.txt
