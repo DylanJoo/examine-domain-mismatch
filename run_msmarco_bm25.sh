6116f54eccb89d225ecfd94397a9f3b8d75565b9
DylanJoo <dylanjootw@gmail.com>
2024-03-16 15:40:43 +0100

    add dr results of msmarco

--- run_msmarco_bm25.sh
+++ run_msmarco_bm25.sh
@@ -1,4 +1,4 @@
-# Indexing
+# Indexing 
 # python -m pyserini.index.lucene \
 #     --collection BeirFlatCollection \
 #     --input /home/dju/datasets/msmarco/collection \
@@ -6,18 +6,20 @@
 #     --generator DefaultLuceneDocumentGenerator \
 #     --threads 4
 
-# Search
+# Searching
 # python retrieval/bm25.py \
 #     --k 1000 --k1 0.82 --b 0.68 \
 #     --index /home/dju/indexes/msmarco.lucene \
 #     --topic /home/dju/datasets/msmarco/queries.dev-subset.txt \
-#     --batch_size 8 \
-#     --output runs/run.msmarco.bm25 \
+#     --batch_size 32 \
+#     --output runs/run.msmarco-dev-subset.bm25 \
 
 # Evaluation
 echo -ne "msmarco-dev-subset | "
 ~/trec_eval-9.0.7/trec_eval \
     -c -m ndcg_cut.10 -m recall.100 \
     /home/dju/datasets/msmarco/qrels.msmarco-passage.dev-subset.txt \
-    runs/run.msmarco.bm25.txt \
+    runs/run.msmarco-dev-subset.bm25.txt \
     | cut -f3 | sed ':a; N; $!ba; s/\n/ | /g'
+
+echo done
